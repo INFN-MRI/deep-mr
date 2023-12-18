b4e3cd5f93ca68554bc4ab0d8d15f2d872d10e6e
Matteo Cencini <matteo.cencini@gmail.com>
2023-12-18 14:11:25 +0100

    Adding virtual object

---
+++ src/deepmr/sim/_virtualobj.py
@@ -0,0 +1,26 @@
+"""
+"""
+import phantominator
+
+
+class VirtualObject:
+    """
+    """
+    def __init__(self):
+        pass
+    
+    def get_echo(self, echo: int):
+        """
+        
+
+        Parameters
+        ----------
+        echo : int
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+
