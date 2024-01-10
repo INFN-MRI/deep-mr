68a8301f134043e82fbfc60520d58b07c4855e7a
Matteo Cencini <matteo.cencini@gmail.com>
2024-01-10 14:48:15 +0100

    Adding optimization routines

---
+++ docs/source/api/optim.md
@@ -0,0 +1,16 @@
+# Iterative Optimization (`deepmr.optim`)
+
+```{eval-rst}
+.. automodule:: deepmr.optim
+```
+## First-order Gradient Methods
+```{eval-rst}
+.. autosummary::
+	:toctree: generated
+	:nosignatures:
+	
+	deepmr.optim.admm
+	deepmr.optim.pgd
+```
+
+## Other Methods
---
+++ src/deepmr/optim/__init__.py
@@ -0,0 +1,17 @@
+"""Sub-package containing optimization routines.
+
+DeepMR provides optimization routines for compressed sensing.
+All the routines are based on the excellent [Deep Inverse](https://github.com/deepinv/deepinv) package.
+
+"""
+
+from . import admm as _admm
+from . import pgd as _pgd
+
+from .admm import * # noqa
+from .pgd import *  # noqa
+
+__all__ = []
+__all__.extend(_admm.__all__)
+__all__.extend(_pgd.__all__)
+
